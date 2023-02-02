b4853b3861a72e6885ede5e74ac412110ed0f6b3
MartinPegeot <martin.pegeot@gmail.com>
2023-02-02 16:17:28 +0100

    Massive modification of storeDiagramInformations in order to get all the relevant infos from a diagram for post-processing. New name: recastDiagramInformations

--- manlab-improvement/Manlab Post-Processing/plotDiagram/TESTplotDiagram.m
+++ manlab-improvement/Manlab Post-Processing/plotDiagram/TESTplotDiagram.m
@@ -9,8 +9,10 @@ pathName = "C:\Users\SONSmanip_001\Documents\Scientifique\Thèse\" + ...
     "Programmation\Systèmes\Van der Pol 5\VdP5 Manlab\" + ...
     "Données Exportées\";
 diagramName = "BD_nu=0.1_sigma=-1.5_beta=1_H=50.mat";
+sysName = "sys_nu=0.1_sigma=-1.5_beta=1_H=50.mat";
 
 diagram = load(strcat(pathName,"Diagrams\",diagramName)).Diagram;
+sys = load(strcat(pathName,"SysODE\",sysName)).sys;
 
-plotDiagram(diagram);
+% plotDiagram(diagram);
 
--- manlab-improvement/Manlab Post-Processing/plotDiagram/plotDiagram.m
+++ manlab-improvement/Manlab Post-Processing/plotDiagram/plotDiagram.m
@@ -1,4 +1,3 @@
-% Main function
 function plotDiagram(diagram)
 
 diagramInformations = storeDiagramInformations(diagram);
@@ -18,47 +17,4 @@ end
 scatter(Xstab,Ystab,'pentagramb')
 hold off
 
-end
-
-% Secondary function
-function diagramInformations = storeDiagramInformations(diagram)
-
-CONTINUITY_LIMIT = 1e-3;
-X_Cell = {0};
-Y_Cell = {0};
-Xstab = [];
-Ystab = [];
-drawtype_Array = [diagram{1}.drawtype_init];
-nDiagram = length(diagram);
-
-
-for iDiagram = 1:nDiagram
-    X = diagram{iDiagram}.X;
-    Y = diagram{iDiagram}.Y;
-    isXContinuous = abs(X_Cell{end}(end)-X(1)) < CONTINUITY_LIMIT;
-    isYContinuous = abs(Y_Cell{end}(end)-Y(1)) < CONTINUITY_LIMIT;
-    isBifurcating = diagram{iDiagram}.Astab > 0;
-    if isXContinuous & isYContinuous & ~isBifurcating
-        X_Cell{end} = [X_Cell{end} ; X];
-        Y_Cell{end} = [Y_Cell{end} ; Y];
-    elseif isBifurcating
-        iBifurcation = diagram{iDiagram}.ind_change;
-        Xstab(end+1) = diagram{iDiagram}.Xstab;
-        Ystab(end+1) = diagram{iDiagram}.Ystab;
-        X_Cell{end} = [X_Cell{end} ; X(1:iBifurcation-1) ; Xstab(end)];
-        Y_Cell{end} = [Y_Cell{end} ; Y(1:iBifurcation-1) ; Ystab(end)];
-        X_Cell{end+1} = [Xstab(end) ; X(iBifurcation:end)];
-        Y_Cell{end+1} = [Ystab(end) ; Y(iBifurcation:end)];
-        drawtype_Array(end+1) = diagram{iDiagram}.drawtype_end;
-    else
-        X_Cell{end+1} = X;
-        Y_Cell{end+1} = Y;
-        drawtype_Array(end+1) = diagram{iDiagram}.drawtype_end;
-    end
-end
-nCurve = length(X_Cell);
-
-diagramInformations = struct("X_Cell",{X_Cell},"Y_Cell",{Y_Cell},...
-    "drawtype_Array",drawtype_Array,"Xstab",Xstab,"Ystab",Ystab,...
-    "nCurve",nCurve);
 end
